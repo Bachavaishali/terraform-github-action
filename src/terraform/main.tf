fc5ef5fed6c1dd60df96cd549680c9df996a6f5e
bachavaishali <bachavaishali@gmail.com>
2023-04-27 09:38:30 +0530

    modified

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -12,10 +12,10 @@ terraform {
 
 provider "azurerm" {
 # Configuration options
-#subscription_id = "6498feeb-1a03-4d38-a1cf-2f21346ef871"
-#tenant_id = "e2998842-9287-4ca9-bfc6-09d7d5aa8407"
-#client_id = "50f0c2a9-3ed5-4c8e-97af-7100c39b6811"
-#client_secret = var.client_secret
+subscription_id = "6498feeb-1a03-4d38-a1cf-2f21346ef871"
+tenant_id = "e2998842-9287-4ca9-bfc6-09d7d5aa8407"
+client_id = "50f0c2a9-3ed5-4c8e-97af-7100c39b6811"
+client_secret = "y9F8Q~uARShtpTHHhiKCOS4NfPrvM~hvoQKSHbI2"
  features {}
 }
 resource "azurerm_resource_group" "example" {
