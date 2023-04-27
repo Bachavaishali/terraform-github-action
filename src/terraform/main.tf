d7d1fbf87812acbe9ebdf459e964ea6bbdf34016
bachavaishali <bachavaishali@gmail.com>
2023-04-27 11:19:07 +0530

    modified

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -1,33 +1,13 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.53.0"
-    }
-  }
-
-
-
-}
-
-provider "azurerm" {
-# Configuration options
-subscription_id = "6498feeb-1a03-4d38-a1cf-2f21346ef871"
-tenant_id = "e2998842-9287-4ca9-bfc6-09d7d5aa8407"
-client_id = "50f0c2a9-3ed5-4c8e-97af-7100c39b6811"
-client_secret = "y9F8Q~uARShtpTHHhiKCOS4NfPrvM~hvoQKSHbI2"
- features {}
-}
 resource "azurerm_resource_group" "example" {
-  name     = "Resource_2"
+  name     = "Resource_3"
   location = "Central India"
 }
 
 
 
 resource "azurerm_storage_account" "example" {
-  name                     = "storage3242345678"
-  resource_group_name      = "Resource_1"
+  name                     = "storage3242345670"
+  resource_group_name      = "Resource_3"
   location                 = "Central India"
   account_tier             = "Standard"
   account_replication_type = "LRS"
@@ -35,7 +15,7 @@ resource "azurerm_storage_account" "example" {
 }
 
 resource "azurerm_storage_account" "example1" {
-  name                     = "storage3242345679"
+  name                     = "storage3242345675"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
