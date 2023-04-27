12e93c364ab4eecb34f401f6e36f037097b41caa
bachavaishali <bachavaishali@gmail.com>
2023-04-27 12:34:48 +0530

    modified

--- src/terraform/backend.tf
+++ src/terraform/backend.tf
@@ -1,8 +1,10 @@
-terraform {
+/* terraform {
   backend "azurerm" {
     ARM_resource_group_name  = "Resource_1"
     ARM_storage_account_name = "storage3242345678"
     ARM_container_name       = "tfstate"
     ARM_key                  = "prod.terraform.tfstate"
   }
-}
+}  */
+
+#
