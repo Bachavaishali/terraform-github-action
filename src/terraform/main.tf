6f8720265ee06d8b6e8a16b54be5a9705cc2656f
bachavaishali <bachavaishali@gmail.com>
2023-04-27 09:07:21 +0530

    modified

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -16,7 +16,7 @@ backend "azurerm" {
 }
 
 provider "azurerm" {
-  # Configuration options
+# Configuration options
  #subscription_id = "6498feeb-1a03-4d38-a1cf-2f21346ef871"
  #tenant_id = "e2998842-9287-4ca9-bfc6-09d7d5aa8407"
  #client_id = "50f0c2a9-3ed5-4c8e-97af-7100c39b6811"
